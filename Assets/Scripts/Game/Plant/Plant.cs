56dbd02fedcf5699ded707fc5d083ff9ed69e788
Archine <2950715596@qq.com>
2023-08-24 19:00:20 +0800

    v 0.4.9通用的植物脚本重构成功

--- Assets/Scripts/Game/Plant/Plant.cs
+++ Assets/Scripts/Game/Plant/Plant.cs
@@ -1,89 +1,108 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using QFramework;
+using Unity.VisualScripting;
 
 namespace IndieFarm
 {
-	public interface IPlant
-	{
-		GameObject GameObject { get; }
-		PlantStates State { get; }
-		void SetState(PlantStates state);
-		void Grow(SoliData soilData);
+    public interface IPlant
+    {
+        GameObject GameObject { get; }
+        PlantStates State { get; }
+        void SetState(PlantStates state);
+        void Grow(SoliData soilData);
 
-		int RipeDay { get; }
-		
-		public int XCell { get; set; }
-		public int YCell { get; set; }
-	}
-	
-	public static class PlantExtentions{
-		
-		public static void ClearSoilDigState(this IPlant self)
-		{//清空地块
-			Object.FindObjectOfType<GridController>().Soil.SetTile(new Vector3Int(self.XCell,self.YCell),null);
-		}
-	}
-	
-	public partial class Plant : ViewController,IPlant
-	{
-		public int RipeDay { get; private set; }
+        int RipeDay { get; }
 
-		public GameObject GameObject => gameObject;
-		
-		public int XCell { get;  set; }
-		public int YCell { get; set; }
-		
-		private PlantStates mState = PlantStates.Seed;
-		public PlantStates State => mState;//get方法
-		
-		public void SetState(PlantStates newState)
-		{
-			if (newState!=mState)
-			{
-				if (mState==PlantStates.Small &&newState ==PlantStates.Ripe)
-				{
-					RipeDay = Global.Days.Value;
-				}
-				mState=newState;
-				//切换表现
-				if (newState ==PlantStates.Small)
-				{
-					this.ClearSoilDigState();
-					GetComponent<SpriteRenderer>().sprite = ResController.Instance.smallPlantSprite;
-				}else if (newState ==PlantStates.Ripe)
-				{
-					GetComponent<SpriteRenderer>().sprite = ResController.Instance.ripeSprite;
-				}else if (newState ==PlantStates.Seed)
-				{
-					GetComponent<SpriteRenderer>().sprite = ResController.Instance.seedSprite;
-				}else if (newState ==PlantStates.Old)
-				{
-					GetComponent<SpriteRenderer>().sprite = ResController.Instance.oldSprite;
-				}
-				//同步到SoilData中
-				FindObjectOfType<GridController>().ShowGrid[XCell, YCell].PlantState = newState;
-			}
-		}
+        public int XCell { get; set; }
+        public int YCell { get; set; }
+    }
 
-		public void Grow(SoliData soilData)
-		{
-			if (State == PlantStates.Seed)
-			{
-			    if (soilData.Watered)
-			    {
-			        //plant切换到small状态
-			        SetState(PlantStates.Small);
-			    }
-			}
-			else if (State == PlantStates.Small)
-			{
-			    if (soilData.Watered)
-			    {
-			       SetState(PlantStates.Ripe);
-			    }
-			}
-		}
+    public static class PlantExtentions
+    {
+        public static void ClearSoilDigState(this IPlant self)
+        {
+            //清空地块
+            Object.FindObjectOfType<GridController>().Soil.SetTile(new Vector3Int(self.XCell, self.YCell), null);
+        }
+    }
 
-	
-	}
+    public partial class Plant : ViewController, IPlant
+    {
+        //可以在Unity的Inspector面板中显示和编辑
+        [System.Serializable]
+        public class PlantState
+        {
+            public PlantStates state;
+            public Sprite sprite;
+            public bool ShowDigState = false;
+            public int Days = 1;
+        }
+
+        public string name;
+        public List<PlantState> states = new List<PlantState>();
+
+        public int RipeDay { get; private set; }
+
+        public GameObject GameObject => gameObject;
+
+        public int XCell { get; set; }
+        public int YCell { get; set; }
+
+        private PlantStates mState = PlantStates.Seed;
+        public PlantStates State => mState; //get方法
+
+        public void SetState(PlantStates newState)
+        {
+            //设置不同阶段的状态
+            if (newState != mState)
+            {
+                //挑战用判断
+                if (mState == PlantStates.Small && newState == PlantStates.Ripe)
+                {
+                    RipeDay = Global.Days.Value;
+                }
+
+                //拿取新阶段的状态
+                var plantState = states.FirstOrDefault(s => s.state == newState);
+                mState = newState;
+
+                if (!plantState.ShowDigState)
+                {
+                    this.ClearSoilDigState();
+                }
+                
+                GetComponent<SpriteRenderer>().sprite = plantState.sprite;
+
+                //同步到SoilData中
+                FindObjectOfType<GridController>().ShowGrid[XCell, YCell].PlantState = newState;
+            }
+        }
+
+        //当前状态的天数
+        private int mDaysInCurrenrState = 0;
+
+        public void Grow(SoliData soilData)
+        {
+            if (mState == PlantStates.Ripe) return;//防止索引越界
+
+            if (soilData.Watered)
+            {
+                //阶段切换
+                mDaysInCurrenrState++;
+                var plantState = states.FirstOrDefault(s => s.state == mState);
+                if (mDaysInCurrenrState >= plantState.Days)
+                {
+                    var currentStateIndex = states.FindIndex(s => s.state == mState);
+                    //获取后一个阶段
+                    currentStateIndex++;
+                    var nextState = states[currentStateIndex];
+                    SetState(nextState.state);
+                    mDaysInCurrenrState = 0;
+                }
+            }
+            
+        }
+    }
 }
